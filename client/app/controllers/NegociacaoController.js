537f6bad02782e0ab8488d612bfc09ef84398038
Rafael Duarte <rduarte@sccalpha.local>
2020-12-05 23:58:22 -0300

    adicionando contexto lexico

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,6 @@
 class NegociacaoController {
 
-    constructor(){
+    constructor() {
         //colocar a lógica de percorrer a DOM dentro do método construtor 
         // é uma boa prática pois fará com que seja percorrido apenas uma vez
         let $ = document.querySelector.bind(document)
@@ -8,40 +8,45 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
         this._form = $('form')
-        
-        this._listaNegociacoes = new ListaNegociacoes()
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._listaNegociacoes = new ListaNegociacoes(this, model =>
+            this._negociacoesView.update(model)
+        )
         this._negociacoesView.update(this._listaNegociacoes)
 
         this._mensagemView = new MensagemView($('#mensagemView'))
         this._mensagem = new Mensagem()
     }
 
-    adiciona(event){
+    adiciona(event) {
 
         event.preventDefault()
-        this._listaNegociacoes._adiciona(this._criacaoNegocio())
+        this._listaNegociacoes.adiciona(this._criacaoNegocio())
         this._mensagem.texto = 'Negociação adicionada com sucesso.'
         this._mensagemView.update(this._mensagem)
         this._negociacoesView.update(this._listaNegociacoes)
         this._resetaForm()
     }
 
-    limparLista(){
+    limparLista() {
         this._listaNegociacoes.esvazia()
-//  this._negociacoesView._update(this._listaNegociacoes)
+        this._negociacoesView.update(this._listaNegociacoes)
+
+        this._mensagem.texto = 'Negociação apagadas com sucesso.'
+        this._mensagemView.update(this._mensagem)
     }
-    
-    _criacaoNegocio(){
+
+    _criacaoNegocio() {
         let date = DateHelper.textoParaData(this._inputData.value)
         return new Negociacoes(
             date,
             this._inputQuantidade.value,
             this._inputValor.value
-            )        
+        )
     }
 
-    _resetaForm(){
+    _resetaForm() {
         this._form.reset()
         this._inputData.focus()
     }
